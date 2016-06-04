79734c2dff92dfbf994c482f98ea3c0e055ee614
hyginn <boris.steipe@utoronto.ca>
2016-06-04 06:20:31 -0400

    Install files and folders

---
+++ .init.R
@@ -0,0 +1,15 @@
+# .init.R
+# Functions to initialize this tutorial session
+# Boris Steipe
+# ====================================================================
+# file.copy(list.files("./assets", full.names=TRUE), tempdir())
+
+# panelViewer <- getOption("viewer")
+
+# ABC.fig <- function(name) {
+#   panelViewer(file.path(tempdir(), name), height=580)
+# }
+
+file.edit("R_Intro-Environment.R")
+
+# [End]
